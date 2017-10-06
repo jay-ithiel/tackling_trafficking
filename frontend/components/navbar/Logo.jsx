1a8a3d320112155a9a5a33f9004bd3bfcc90b436
Jay Hwang <jayhwang0121@gmail.com>
2017-10-06 16:33:23 -0700

    Made Logo component skeleton for Navbar

---
+++ frontend/components/navbar/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Logo = () => (
+  <Link to='/'>
+    <img id='Logo' className='btn' alt='Logo' src=''/>
+  </Link>
+);
+
+export default Logo;
--- frontend/components/navbar/Navbar.jsx
+++ frontend/components/navbar/Navbar.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export default class Navbar extends React.Component {
+// Components
+import Logo from './Logo';
+
+class Navbar extends React.Component {
   render() {
     return (
       <nav id="Navbar">
-
+        <Logo/>
+        
       </nav>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  loggedIn: state.users.currentUser ? true : false,
+  currentUser: state.users.currentUser
+});
+
+export default connect(mapStateToProps, null)(Navbar);
