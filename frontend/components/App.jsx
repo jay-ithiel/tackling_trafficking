c52d0ee285ad62ac86c8774915197daa8dd3f5c6
Jay Hwang <jayhwang0121@gmail.com>
2017-10-06 15:44:21 -0700

    Created splash component

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Switch, withRouter, Route } from 'react-router-dom';
 
 // Components
+import Splash from './Splash';
 
 const App = () => (
   <div id="App">
     <Switch>
-      
+      <Route exact path='/' component={Splash}></Route>
     </Switch>
   </div>
 );
---
+++ frontend/components/Splash.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Components
+import Layout from './Layout';
+
+const Splash = () => (
+  <Layout id="Splash">
+
+  </Layout>
+);
+
+export default Splash;
