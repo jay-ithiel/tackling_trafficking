5973f3407542fa5951466a9e94c2758f265c91dd
Jay Hwang <jayhwang0121@gmail.com>
2017-10-06 13:46:11 -0700

    Made sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login!(@user)
+      render 'api/users/show'
+    else
+      render json: ["Invalid login credentials"], status: 422
+    end
+  end
+
+  def destroy
+    if logged_in?
+      logout!
+      render json: {}
+    else
+      render json: ["No current user"], status: 404
+    end
+  end
+end
